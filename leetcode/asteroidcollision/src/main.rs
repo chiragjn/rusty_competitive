fb3cdf7555fa99d8e0571604660c18b857e4cb06
Chirag Jain <jain.chirag925@gmail.com>
2020-10-22 10:06:30 +0530

    Add Asteroid Collision

--- leetcode/asteroidcollision/src/main.rs
+++ leetcode/asteroidcollision/src/main.rs
@@ -1,8 +1,36 @@
+// https://leetcode.com/problems/asteroid-collision/
+
+use std::cmp::Ordering;
+
 struct Solution {}
 
 impl Solution {
     pub fn asteroid_collision(asteroids: Vec<i32>) -> Vec<i32> {
-        return vec![];
+        let mut buffer: Vec<i32> = vec![];
+        for &x in asteroids.iter() {
+            let mut destroyed = false;
+            while !buffer.is_empty() && x < 0 && *buffer.last().unwrap() > 0 {
+                let last = buffer.last().unwrap();
+                match x.abs().cmp(&last.abs()) {
+                    Ordering::Equal => {
+                        destroyed = true;
+                        buffer.pop();
+                        break;
+                    }
+                    Ordering::Less => {
+                        destroyed = true;
+                        break;
+                    }
+                    _ => {
+                        buffer.pop();
+                    }
+                }
+            }
+            if !destroyed {
+                buffer.push(x);
+            }
+        }
+        return buffer;
     }
 }
 
