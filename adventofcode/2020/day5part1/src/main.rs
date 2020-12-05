7aaf4999dacd291b641d532c3b7e608c47dd42ab
Chirag Jain <jain.chirag925@gmail.com>
2020-12-05 12:06:40 +0530

    Add Day 5 of AoC 2020

---
+++ adventofcode/2020/day5part1/src/main.rs
@@ -0,0 +1,76 @@
+use std::io::{self, BufRead, Stdin};
+
+struct InputUtils {
+    stream: Stdin,
+    buffer: String,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        return Self {
+            stream: io::stdin(),
+            buffer: String::new(),
+        };
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stream.lock().lines().next() {
+            Some(line) => Some(line.unwrap().trim().to_string()),
+            None => None,
+        }
+    }
+}
+
+fn bsp_decode(code: &[char]) -> i64 {
+    let mut low = 0;
+    let mut high = (1 << code.len()) - 1;
+    for &c in code.iter() {
+        let mid = low + ((high - low) / 2);
+        if c == 'L' || c == 'F' {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    assert!(low == high);
+    return low as i64;
+}
+
+fn decode(seat_code: String) -> i64 {
+    let seat_code: Vec<char> = seat_code.chars().collect();
+    return bsp_decode(&seat_code[0..7]) * 8 + bsp_decode(&seat_code[7..10]);
+}
+
+fn solve(lines: Box<dyn Iterator<Item = String>>) -> i64 {
+    return lines
+        .map(|line| decode(line))
+        .max()
+        .expect("No seat codes to decode!");
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test() {
+        let test_input = r#"BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 820);
+    }
+}
