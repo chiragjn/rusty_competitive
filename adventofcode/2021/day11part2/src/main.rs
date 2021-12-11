29e9e074c3cb2ab2cf498cbc0c4c762c2deb0168
Chirag Jain <jain.chirag925@gmail.com>
2021-12-11 21:35:48 +0530

    Add advent of code 2021 day 11

---
+++ adventofcode/2021/day11part2/src/main.rs
@@ -0,0 +1,126 @@
+use std::collections::HashSet;
+use std::io::{self, BufRead, Stdin};
+
+struct InputUtils {
+    stream: Stdin,
+    buffer: String,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        return Self {
+            stream: io::stdin(),
+            buffer: String::new(),
+        };
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stream.lock().lines().next() {
+            Some(line) => Some(line.unwrap().trim().to_string()),
+            None => None,
+        }
+    }
+}
+
+fn simulate(grid: &mut Vec<Vec<u8>>) -> u64 {
+    let neighbors: [(isize, isize); 8] = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+    let mut buffer: Vec<(usize, usize)> = vec![];
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut flashes = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            grid[i][j] = (grid[i][j] + 1) % 10;
+        }
+    }
+    loop {
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
+                if grid[i][j] == 0 && !visited.contains(&(i, j)) {
+                    visited.insert((i, j));
+                    buffer.push((i, j));
+                }
+            }
+        }
+        if buffer.is_empty() {
+            break;
+        }
+        flashes += buffer.len();
+        while let Some((i, j)) = buffer.pop() {
+            for &(offx, offy) in neighbors.iter() {
+                let x = i as isize + offx;
+                let y = j as isize + offy;
+                if x >= 0 && x < (grid.len() as isize) {
+                    let x = x as usize;
+                    if y >= 0 && y < (grid[x as usize].len() as isize) {
+                        let y = y as usize;
+                        if grid[x][y] != 0 {
+                            grid[x][y] = (grid[x][y] + 1) % 10;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return flashes as u64;
+}
+
+fn solve(lines: Box<dyn Iterator<Item = String>>) -> u64 {
+    let mut grid: Vec<Vec<u8>> = vec![];
+    for line in lines {
+        grid.push(
+            line.chars()
+                .map(|c| c.to_digit(10).expect("Failed to cast char to digit") as u8)
+                .collect(),
+        );
+    }
+    let mut day = 0;
+    loop {
+        day += 1;
+        if simulate(&mut grid) == 100 {
+            return day;
+        }
+    }
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test() {
+        let test_input = r#"5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 195);
+    }
+}
