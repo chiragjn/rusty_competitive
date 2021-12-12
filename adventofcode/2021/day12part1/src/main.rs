fd26f4879d401bdf3a0db6b744abc45cd19753ba
Chirag Jain <jain.chirag925@gmail.com>
2021-12-12 21:01:46 +0530

    Add advent of code 2021 day 12

---
+++ adventofcode/2021/day12part1/src/main.rs
@@ -0,0 +1,170 @@
+use std::collections::{HashMap, HashSet};
+use std::io::{self, BufRead, Stdin};
+
+struct InputUtils {
+    stream: Stdin,
+    buffer: String,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        return Self {
+            stream: io::stdin(),
+            buffer: String::new(),
+        };
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stream.lock().lines().next() {
+            Some(line) => Some(line.unwrap().trim().to_string()),
+            None => None,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum Node<'a> {
+    START,
+    UPPER(&'a str),
+    LOWER(&'a str),
+    END,
+}
+
+fn to_node<'a>(s: &'a str) -> Node<'a> {
+    if s == "start" {
+        return Node::START;
+    } else if s == "end" {
+        return Node::END;
+    } else if s.to_uppercase() == s {
+        return Node::UPPER(s);
+    } else {
+        return Node::LOWER(s);
+    }
+}
+
+// lifetimes madness
+fn traverse<'a>(
+    node: &'a Node<'a>,
+    visited: &mut HashSet<&'a Node<'a>>,
+    graph: &'a HashMap<Node<'a>, Vec<Node<'a>>>,
+) -> u64 {
+    if *node == Node::END {
+        return 1;
+    } else {
+        if let Node::LOWER(_) | Node::START = node {
+            visited.insert(node);
+        }
+
+        let mut paths = 0;
+        for neighbor in graph[node].iter() {
+            if !visited.contains(neighbor) {
+                paths += traverse(neighbor, visited, graph);
+            }
+        }
+
+        if let Node::LOWER(_) | Node::START = node {
+            visited.remove(node);
+        }
+
+        return paths;
+    }
+}
+
+fn solve(lines: Box<dyn Iterator<Item = String>>) -> u64 {
+    let mut graph: HashMap<Node, Vec<Node>> = HashMap::new();
+    let lines: Vec<String> = lines.collect();
+    for line in lines.iter() {
+        let mut parts = line.trim().split('-');
+        let u = to_node(
+            parts
+                .next()
+                .expect(&format!("Failed to split by line {} '-'", line)),
+        );
+        let v = to_node(
+            parts
+                .next()
+                .expect(&format!("Failed to split by line {} '-'", line)),
+        );
+        graph.entry(u.clone()).or_default().push(v.clone());
+        graph.entry(v).or_default().push(u);
+    }
+    let mut visited: HashSet<&Node> = HashSet::new();
+    return traverse(&Node::START, &mut visited, &graph);
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test_small() {
+        let test_input = r#"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 10);
+    }
+
+    #[test]
+    fn test_medium() {
+        let test_input = r#"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 19);
+    }
+
+    #[test]
+    fn test_big() {
+        let test_input = r#"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 226);
+    }
+}
---
+++ adventofcode/2021/day12part2/src/main.rs
@@ -0,0 +1,228 @@
+use std::collections::{HashMap, HashSet};
+use std::io::{self, BufRead, Stdin};
+
+struct InputUtils {
+    stream: Stdin,
+    buffer: String,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        return Self {
+            stream: io::stdin(),
+            buffer: String::new(),
+        };
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stream.lock().lines().next() {
+            Some(line) => Some(line.unwrap().trim().to_string()),
+            None => None,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum Node<'a> {
+    START,
+    UPPER(&'a str),
+    LOWER(&'a str),
+    END,
+    PLACEHOLDER,
+}
+
+fn to_node<'a>(s: &'a str) -> Node<'a> {
+    if s == "start" {
+        return Node::START;
+    } else if s == "end" {
+        return Node::END;
+    } else if s.to_uppercase() == s {
+        return Node::UPPER(s);
+    } else {
+        return Node::LOWER(s);
+    }
+}
+
+fn node_to_str<'a>(node: &'a Node) -> &'a str {
+    match node {
+        Node::START => {
+            return "start";
+        }
+        Node::END => {
+            return "end";
+        }
+        Node::LOWER(x) | Node::UPPER(x) => {
+            return x;
+        }
+        Node::PLACEHOLDER => {
+            unreachable!("Node::PLACEHOLDER not supported");
+        }
+    }
+}
+
+// lifetimes madness
+struct PathsCounter<'a> {
+    graph: &'a HashMap<Node<'a>, Vec<Node<'a>>>,
+    visited: HashSet<&'a Node<'a>>,
+    twice_allowed_for: &'a Node<'a>,
+    visited_count: usize,
+    buffer: Vec<&'a str>,
+    paths: HashSet<String>,
+}
+
+impl<'a> PathsCounter<'a> {
+    fn new(graph: &'a HashMap<Node<'a>, Vec<Node<'a>>>) -> Self {
+        return PathsCounter {
+            graph,
+            visited: HashSet::new(),
+            twice_allowed_for: &Node::PLACEHOLDER,
+            visited_count: 0,
+            buffer: vec![],
+            paths: HashSet::new(),
+        };
+    }
+
+    fn _traverse(&mut self, node: &'a Node<'a>) -> u64 {
+        if *node == Node::END {
+            self.paths.insert(self.buffer.join("-"));
+            return 1;
+        } else {
+            self.buffer.push(node_to_str(node));
+            if node == self.twice_allowed_for {
+                self.visited_count += 1;
+                if self.visited_count == 2 {
+                    self.visited.insert(node);
+                }
+            } else if let Node::LOWER(_) | Node::START = node {
+                self.visited.insert(node);
+            }
+
+            let mut paths = 0;
+            for neighbor in self.graph[node].iter() {
+                if !self.visited.contains(neighbor) {
+                    paths += self._traverse(neighbor);
+                }
+            }
+
+            if node == self.twice_allowed_for {
+                self.visited_count -= 1;
+                self.visited.remove(node);
+            } else if let Node::LOWER(_) | Node::START = node {
+                self.visited.remove(node);
+            }
+
+            self.buffer.pop();
+
+            return paths;
+        }
+    }
+
+    fn count(&mut self) -> u64 {
+        for node in self.graph.keys() {
+            if let Node::LOWER(_) = node {
+                self.twice_allowed_for = node;
+                self.visited_count = 0;
+                self._traverse(&Node::START);
+            }
+        }
+        return self.paths.len() as u64;
+    }
+}
+
+fn solve(lines: Box<dyn Iterator<Item = String>>) -> u64 {
+    let lines: Vec<String> = lines.collect();
+    let mut graph: HashMap<Node, Vec<Node>> = HashMap::new();
+    for line in lines.iter() {
+        let mut parts = line.trim().split('-');
+        let u = to_node(
+            parts
+                .next()
+                .expect(&format!("Failed to split by line {} '-'", line)),
+        );
+        let v = to_node(
+            parts
+                .next()
+                .expect(&format!("Failed to split by line {} '-'", line)),
+        );
+        graph.entry(u.clone()).or_default().push(v.clone());
+        graph.entry(v).or_default().push(u);
+    }
+    return PathsCounter::new(&graph).count();
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test_small() {
+        let test_input = r#"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 36);
+    }
+
+    #[test]
+    fn test_medium() {
+        let test_input = r#"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 103);
+    }
+
+    #[test]
+    fn test_big() {
+        let test_input = r#"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 3509);
+    }
+}
