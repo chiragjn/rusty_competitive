336bc3b10186562fc44960970c1a9672fe3b1dd7
Chirag Jain <jain.chirag925@gmail.com>
2022-12-06 11:28:39 +0530

    Add Day 6 for 2022

---
+++ adventofcode/2022/day06part1/src/main.rs
@@ -0,0 +1,73 @@
+use std::{
+    collections::HashMap,
+    io::{self, BufRead, Stdin},
+};
+
+struct InputUtils {
+    stream: Stdin,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        Self {
+            stream: io::stdin(),
+        }
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stream
+            .lock()
+            .lines()
+            .next()
+            .map(|line| line.unwrap().trim().to_string())
+    }
+}
+
+fn decrement_remove(chars: &mut HashMap<char, u32>, c: char) {
+    *chars
+        .get_mut(&c)
+        .unwrap_or_else(|| panic!("Map does not contain {c}")) -= 1;
+    if *chars.get(&c).unwrap() == 0 {
+        chars.remove(&c);
+    }
+}
+
+fn solve(mut lines: Box<dyn Iterator<Item = String>>) -> u64 {
+    let mut map: HashMap<char, u32> = HashMap::new();
+    let chars: Vec<char> = lines.next().expect("Input is empty!").chars().collect();
+    for (i, c) in chars.iter().enumerate() {
+        *map.entry(*c).or_insert(0) += 1;
+        if i >= 4 {
+            decrement_remove(&mut map, chars[i - 4]);
+        }
+        if i >= 3 && map.len() == 4 {
+            return (i + 1) as u64;
+        }
+    }
+    0
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test() {
+        let test_input = r#"mjqjpqmgbljsphdztnvjfqwrcgsmlb"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 7);
+    }
+}
