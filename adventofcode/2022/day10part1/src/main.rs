68d20e8a275ce58bb10593a3f9c21de8e6e8e5fa
Chirag Jain <jain.chirag925@gmail.com>
2022-12-25 02:49:12 +0530

    Add Day 10 for 2022

---
+++ adventofcode/2022/day10part1/src/main.rs
@@ -0,0 +1,229 @@
+use std::io::{self, BufRead, Stdin};
+
+struct InputUtils {
+    stream: Stdin,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        Self {
+            stream: io::stdin(),
+        }
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stream
+            .lock()
+            .lines()
+            .next()
+            .map(|line| line.unwrap().trim().to_string())
+    }
+}
+
+fn process_tick(tick: &mut i64, x: &i64) -> i64 {
+    *tick += 1;
+    if *tick > 220 {
+        return 0;
+    }
+    if (*tick - 20) % 40 == 0 {
+        return *tick * *x;
+    }
+    0
+}
+
+fn solve(lines: Box<dyn Iterator<Item = String>>) -> i64 {
+    let mut x: i64 = 1;
+    let mut tick: i64 = 0;
+    let mut sum: i64 = 0;
+    for line in lines {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        match parts[0] {
+            "noop" => {
+                sum += process_tick(&mut tick, &x);
+            }
+            "addx" => {
+                let value = parts
+                    .last()
+                    .and_then(|part| part.parse::<i64>().ok())
+                    .expect("second argument to addx must be an integer");
+                sum += process_tick(&mut tick, &x);
+                sum += process_tick(&mut tick, &x);
+                x += value;
+            }
+            _ => {
+                unreachable!("Invalid instruction!")
+            }
+        }
+    }
+    sum
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test() {
+        let test_input = r#"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), 13140);
+    }
+}
---
+++ adventofcode/2022/day10part2/src/main.rs
@@ -0,0 +1,238 @@
+use std::io::{self, BufRead, Stdin};
+
+struct InputUtils {
+    stream: Stdin,
+}
+
+impl Default for InputUtils {
+    fn default() -> Self {
+        Self {
+            stream: io::stdin(),
+        }
+    }
+}
+
+impl Iterator for InputUtils {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stream
+            .lock()
+            .lines()
+            .next()
+            .map(|line| line.unwrap().trim().to_string())
+    }
+}
+
+fn process_tick(tick: &mut i64, x: &i64, screen: &mut String) {
+    let draw_pos = *tick % 40;
+    if (x - draw_pos).abs() <= 1 {
+        screen.push('#');
+    } else {
+        screen.push('.');
+    }
+    *tick += 1;
+    if *tick % 40 == 0 {
+        screen.push('\n');
+    }
+}
+
+fn solve(lines: Box<dyn Iterator<Item = String>>) -> String {
+    let mut x: i64 = 1;
+    let mut tick: i64 = 0;
+    let mut screen: String = String::new();
+    for line in lines {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        match parts[0] {
+            "noop" => {
+                process_tick(&mut tick, &x, &mut screen);
+            }
+            "addx" => {
+                let value = parts
+                    .last()
+                    .and_then(|part| part.parse::<i64>().ok())
+                    .expect("second argument to addx must be an integer");
+                process_tick(&mut tick, &x, &mut screen);
+                process_tick(&mut tick, &x, &mut screen);
+                x += value;
+            }
+            _ => {
+                unreachable!("Invalid instruction!")
+            }
+        }
+    }
+    screen
+}
+
+fn main() {
+    let iu = InputUtils::default();
+    let boxed_iter = Box::new(iu);
+    println!("{}", solve(boxed_iter));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn test() {
+        let test_input = r#"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"#;
+        let test_output = r#"##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+"#;
+        let it = test_input
+            .split('\n')
+            .into_iter()
+            .map(|part| part.to_string());
+        assert_eq!(solve(Box::new(it)), String::from(test_output));
+    }
+}
